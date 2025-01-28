9c125df7b5f65bd5010e643e81a240f168437a5b
anaisrandria <anais.randriamitantsoa@gmail.com>
2025-01-28 14:54:10 +0100

    fix: uncomment return

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
-// --------- ETAPE 1 --------- // 
+// --------- ETAPE 1 --------- //
 function decoupeChaine(str) {
-    const arr = str.split('')
-    let newStr = ""
+	const arr = str.split("");
+	let newStr = "";
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i + 1]) {
-            newStr += arr[i]
-        } else {
-            newStr += arr[i] + " "
-        }
-    }
-    console.log(newStr)
-}
-
-decoupeChaine("ab")     // renvoie "a b"
-decoupeChaine("aabbca") // renvoie "aa bb c a"`
-
-
-// --------- ETAPE 2 --------- // 
-function decritChaine(str) {
-    const arr = str.split('')
-    let newStr = ""
-    let count = 1
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i + 1]) {
-            count ++;
-        } else {
-            newStr += count + arr[i];
-            count = 1;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === arr[i + 1]) {
+			newStr += arr[i];
+		} else {
+			newStr += arr[i] + " ";
 		}
-    }
-    // console.log(newStr);
-    return newStr
+	}
+	console.log(newStr);
 }
 
-decritChaine("ab")      // renvoie "1a1b"
-decritChaine("aabbca")  // renvoie "2a2b1c1a"
+decoupeChaine("ab"); // renvoie "a b"
+decoupeChaine("aabbca"); // renvoie "aa bb c a"`
 
+// --------- ETAPE 2 --------- //
+function decritChaine(str) {
+	const arr = str.split("");
+	let newStr = "";
+	let count = 1;
 
-// --------- ETAPE 3 --------- // 
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === arr[i + 1]) {
+			count++;
+		} else {
+			newStr += count + arr[i];
+			count = 1;
+		}
+	}
+	console.log(newStr);
+	return newStr;
+}
+
+decritChaine("ab"); // renvoie "1a1b"
+decritChaine("aabbca"); // renvoie "2a2b1c1a"
+
+// --------- ETAPE 3 --------- //
 function suiteConway(carac, n) {
-    let str = ""
-    str += carac
-    console.log(str)
+	let str = "";
+	str += carac;
+	console.log(str);
 
-    for (let i = 0; i < n - 1; i++) {
-        str = decritChaine(str)
-        console.log(str)
-    }
-
+	for (let i = 0; i < n - 1; i++) {
+		str = decritChaine(str);
+	}
 }
 
-suiteConway('a', 3)  
+suiteConway("a", 3);
 // a
 // 1a
 // 111a
 
-suiteConway('1', 3)  
+suiteConway("1", 3);
 // 1
 // 11
 // 21
+
+
+// --------- ETAPE 4 --------- //
+// const char = document.getElementById("char");
+// const n = document.getElementById("n");
+// const button = document.getElementById("button");
+// const result = document.getElementById("result");
+
+// button.addEventListener("click", () => {
+//     result.innerHTML = suiteConway(char.value, n.value);
+// })
